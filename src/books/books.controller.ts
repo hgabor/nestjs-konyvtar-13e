350e99e3e57ae1843d5dbe49f92cef4924defcf7
Gabor Halasz <gabor.halasz@level14.hu>
2024-11-07 13:41:00 +0100

    GET endpoints

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -19,7 +19,11 @@ export class BooksController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.booksService.findOne(+id);
+    const book = this.booksService.findOne(+id);
+    if (!book) {
+      throw new NotFoundException('No book with this id');
+    }
+    return book;
   }
 
   @Patch(':id')
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -5,19 +5,44 @@ import { Book } from './entities/book.entity';
 
 @Injectable()
 export class BooksService {
-  books: Book[] = [];
-  nextId = 1;
+  books: Book[] = [
+    {
+      id: 1,
+      author: 'A.A. Milne',
+      title: 'Winnie the Pooh',
+      isbn: '0525444432',
+      publishYear: 1988,
+      reserved: true,
+    },
+        {
+      id: 2,
+      author: 'A.A. Milne',
+      title: 'Winnie the Pooh 2 - The Sequel',
+      isbn: '978-3-16-148410-0',
+      publishYear: 2001,
+      reserved: false,
+    },
+    {
+      id: 4,
+      author: 'Me',
+      title: 'Learn NestJS in 30 days',
+      isbn: '978-3-16-148410-0',
+      publishYear: 2023,
+      reserved: false,
+    },
+  ];
+  nextId = 5;
 
   create(createBookDto: CreateBookDto) {
     return 'This action adds a new book';
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.books;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return this.books.find(book => book.id == id);
   }
 
   update(id: number, updateBookDto: UpdateBookDto) {
